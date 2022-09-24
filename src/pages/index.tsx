ed0eb25d093862b7bd1539e756626b619f2c976a
Masato Ohira <ohira.ekzm@gmail.com>
2022-09-25 02:15:14 +0900

    add:useFetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,48 +4,62 @@ import { css } from '@emotion/react'
 import { colors, mq } from '@/styles/cmn.styles'
 import { useSelector } from 'react-redux'
 import { siteData } from '@/store/site'
-
-const styles = () => {
-  return css`
-    .box {
-      box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 0 0;
-
-      .button {
-        background-color: ${colors.link};
-        border-color: ${colors.link};
-        color: #fff;
-        transition-property: all;
-        transition-duration: 0.2s;
-        transition-timing-function: linear;
-
-        ${mq('lg')} {
-          background-color: ${colors.primary};
-          border-color: ${colors.primary};
-        }
-
-        &:hover {
-          opacity: 0.8;
-        }
-      }
-    }
-  `
-}
+import useFetch from '@/hooks/useFetch'
+import LoaderContent from '@/components/loader-content'
 
 const Home: NextPage = () => {
   const { name: siteName } = useSelector(siteData)
+  const styles = {
+    container: css`
+      .box {
+        box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 0 0;
+
+        .button {
+          background-color: ${colors.link};
+          border-color: ${colors.link};
+          color: #fff;
+          transition-property: all;
+          transition-duration: 0.2s;
+          transition-timing-function: linear;
+
+          ${mq('lg')} {
+            background-color: ${colors.primary};
+            border-color: ${colors.primary};
+          }
+
+          &:hover {
+            opacity: 0.8;
+          }
+        }
+      }
+    `,
+  }
+
+  const url = `https://jsonplaceholder.typicode.com/todos`
+  const { data, isLoading, error } = useFetch(url, {})
+
+  if (isLoading) return <LoaderContent />
+  if (error) return <p>{'An error has occurred: ' + error.message}</p>
 
   return (
     <>
       <Head>
         <title>{siteName}</title>
       </Head>
-      <div className='container' css={styles()}>
-        <div className='box'>
-          <div className='title is-5 has-text-centered'>{siteName}</div>
-          <div className='buttons is-centered'>
-            <div className='button'>BUTTON</div>
-          </div>
-        </div>
+      <div className='container' css={styles.container}>
+        <div className='title is-5 has-text-centered'>{siteName}</div>
+        <table className='table is-bordered is-fullwidth'>
+          <tbody>
+            {data.map((i: any, key: number) => {
+              return (
+                <tr key={key}>
+                  <td>{i.id}</td>
+                  <td>{i.title}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     </>
   )
