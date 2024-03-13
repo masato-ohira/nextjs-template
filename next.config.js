00d049e03db7893bfe9876b23368f09bcc394724
Masato Ohira <ohira.ekzm@gmail.com>
2024-03-13 22:04:24 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const pkg = require('./package.json')
 const isDev = process.env.NODE_ENV == 'development'
 
 const nextConfig = {
-  env: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_KEY: process.env.SUPABASE_KEY,
-  },
   reactStrictMode: false,
   output: 'export',
   distDir: isDev ? undefined : `./dist/${pkg.config.base}`,
